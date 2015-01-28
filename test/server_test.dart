cada47e68c0cfacfd26aa7dca876fd99f9bed6e8
Brian Grey <mixolyde@gmail.com>
2015-01-27 23:31:57 -0500

    Added first real server test

--- test/server_test.dart
+++ test/server_test.dart
@@ -1,11 +1,34 @@
 library server_test;
 
+import 'dart:convert';
+
+import 'package:redstone/mocks.dart';
+import 'package:redstone/server.dart' as app;
 import 'package:unittest/unittest.dart';
 
+import '../declarations.dart';
 
 main() {  
-  test("true equals true", () {  
-   expect(true, equals(true));  
-  });  
+  
+  //load handlers in 'authServer' library
+  setUp(() => app.setUp([#coUserver]));
+
+  //remove all loaded handlers
+  tearDown(() => app.tearDown());
+  
+  test("GET server status", () {
+    //create a mock request
+    var req = new MockRequest("/serverStatus");
+    //dispatch the request
+    return app.dispatch(req).then((resp) {
+      //verify the response
+      expect(resp.statusCode, equals(200));
+      var content = JSON.decode(resp.mockContent);
+      expect(content, containsPair("playerList", []));
+      expect(content, containsPair("numStreetsLoaded", 0));
+    });
+  });
+
+  
  }  
 
